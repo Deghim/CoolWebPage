47389951e846a423885841c697369119f59ff42a
Jorge Chavira <jorgechavira4@gmail.com>
2025-04-27 17:57:31 -0600

    Trackeo del scroll funcionando

--- cool-webpage/src/app/page.tsx
+++ cool-webpage/src/app/page.tsx
@@ -5,23 +5,28 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   const [progreView, setProgreView] = useState(0);
-  
+
   useEffect(() => {
-    setProgreView(0);
+    const updateProgre = () => {
+      const scrollY: number = window.scrollY
+      const viewPortH: number = window.innerHeight
+
+      const raw: number = (scrollY / viewPortH) * 100
+
+      const pct: number = Math.min(Math.max(raw, 0), 100);
+
+      setProgreView(Math.round(pct))
+    }
+
+    window.addEventListener("scroll", updateProgre, { passive: true })
+    updateProgre()
+
+    return () => window.removeEventListener("scroll", updateProgre)
   }, []);
-  
+
   return (
     <div className="relative min-h-screen">
-      <div 
-        style={{
-          position: 'fixed',
-          bottom: '32px',
-          left: '50%',
-          transform: 'translateX(-50%)',
-          fontSize: '1rem',
-          fontWeight: 'bold',
-        }}
-      >
+      <div style={{ position: 'fixed', bottom: '32px', left: '50%', transform: 'translateX(-50%)', fontSize: '1rem', fontWeight: 'bold', }}>
         {progreView}%
       </div>
       <Landing />
