7a81c3833423e4c1233de45b0b38efb929ea2cc9
Jorge Chavira <jorgechavira4@gmail.com>
2025-04-27 14:36:21 -0600

    Secciones de la landing

--- cool-webpage/src/app/pages/landing.tsx
+++ cool-webpage/src/app/pages/landing.tsx
@@ -3,23 +3,33 @@ import { TypeAnimation } from "react-type-animation";
 
 export default function Landing() {
     return (
-        <div className="w-full min-h-screen flex items-center justify-center ">
-            <div className="flex items-center space-x-4 gap-2">
-                <p className="text-6xl font-bold ">Hi,</p>
-                <span className="bg-yellow-300 dark:bg-red-600">
-                    <TypeAnimation
-                        sequence={[
-                            "user!", 2000,
-                            "developer!", 2000,
-                            "mom!", 2000
-                        ]}
-                        wrapper="span"
-                        speed={10}
-                        className="text-6xl font-bold"
-                        repeat={Infinity}
-                    />
-                </span>
-            </div>
-        </div>
+        <>
+
+            <section className="w-full min-h-screen flex items-center justify-center max-h-screen">
+                <div className="flex items-center space-x-4 gap-2">
+                    <p className="text-6xl font-bold ">Hi,</p>
+                    <span className="bg-yellow-300 dark:bg-red-600">
+                        <TypeAnimation
+                            sequence={[
+                                "user!", 2000,
+                                "developer!", 2000,
+                                "mom!", 2000
+                            ]}
+                            wrapper="span"
+                            speed={10}
+                            className="text-6xl font-bold"
+                            repeat={Infinity}
+                        />
+                    </span>
+                </div>
+            </section>
+            <section className="w-full min-h-screen flex items-center justify-center max-h-screen">
+                <div className="flex items space-x-4 gap-10">
+                    <p className="text-6xl font-bold" >Dev. Exp.</p>
+                    <p className="text-6xl font-bold">User Exp.</p>
+
+                </div>
+            </section>
+        </>
     );
 }
