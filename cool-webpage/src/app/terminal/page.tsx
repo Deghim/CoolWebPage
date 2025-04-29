ac03265a33d942d7ecc50215a1eb88902bb53b3d
Jorge Chavira <jorgechavira4@gmail.com>
2025-04-29 14:09:33 -0600

    Primera instancia de la creacion de la pagina de la terminal

--- cool-webpage/src/app/terminal/page.tsx
+++ cool-webpage/src/app/terminal/page.tsx
@@ -1,5 +1,37 @@
+"use client";
+import Link from "next/link";
+import { useState, useEffect } from "react";
+import PixelBack from "../pixelBackground/pixelBackground";
+
+
 export default function Terminal() {
+    const [showAnimation, setShowAnimation] = useState(true)
+    const [gatillo, setGatillo] = useState(false);
+
+    useEffect(() => {
+        setGatillo(!false)
+        setTimeout(() => {
+            setTimeout(() => {
+                setShowAnimation(false)
+            }, 1000)
+
+        }, 500)
+
+    }, []);
+
+
     return (
-        <p>Hola mama</p>
+        <>
+            {showAnimation &&
+                <div className="fixed inset-0 z-10" >
+                    <PixelBack gatillo={!gatillo} />
+                </div>
+
+            }
+            <p>Hola mama</p>
+           <Link href={"/"}>
+           <button >Soy un boton</button>
+           </Link>
+        </>
     )
 }
