d779763ba8984b071ef62d52dcb706028336635e
Jorge Chavira <jorgechavira4@gmail.com>
2025-04-30 12:26:59 -0600

    coneccion de rutas de la pagina

--- cool-webpage/src/app/options/options.tsx
+++ cool-webpage/src/app/options/options.tsx
@@ -1,6 +1,5 @@
 "use client";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 import { CSSProperties, useState } from "react";
 
 interface OptionsProps {
@@ -8,7 +7,6 @@ interface OptionsProps {
 }
 
 export default function Options({ opacity }: OptionsProps) {
-    const router = useRouter()
     const op = Math.min(Math.max(opacity, 0), 100) / 100;
     const [hoverDev, setHoverDev] = useState(false);
     const [hoverUser, setHoverUser] = useState(false);
@@ -46,62 +44,62 @@ export default function Options({ opacity }: OptionsProps) {
         >
             <div className="flex items-center space-x-4 gap-10">
                 <Link href={"/terminal"}>
-                <button className="text-6xl font-bold"
-                    style={{
-                        ...buttonBaseStyle,
-                    }}
-                    onMouseEnter={() => setHoverDev(true)}
-                    onMouseLeave={() => setHoverDev(false)}
-                    onClick={() => router.push("/terminal")}
-                >
-                    <svg style={svgStyle}>
-                        <rect
-                            width="100%"
-                            height="100%"
-                            rx="10"
-                            ry="10"
-                            style={{
-                                fill: 'none',
-                                stroke: 'currentColor',
-                                strokeWidth: '4',
-                                strokeDasharray: hoverDev ? '400 0' : '20 320',
-                                strokeDashoffset: hoverDev ? 40 : 100,
-                                transition: 'all 1.35s cubic-bezier(0.15, 1, 0.22, 1)'
-                            }}
-                        />
-                    </svg>
-                    Dev. Exp.
-                </button>
+                    <button className="text-6xl font-bold"
+                        style={{
+                            ...buttonBaseStyle,
+                        }}
+                        onMouseEnter={() => setHoverDev(true)}
+                        onMouseLeave={() => setHoverDev(false)}
+                    >
+                        <svg style={svgStyle}>
+                            <rect
+                                width="100%"
+                                height="100%"
+                                rx="10"
+                                ry="10"
+                                style={{
+                                    fill: 'none',
+                                    stroke: 'currentColor',
+                                    strokeWidth: '4',
+                                    strokeDasharray: hoverDev ? '400 0' : '20 320',
+                                    strokeDashoffset: hoverDev ? 40 : 100,
+                                    transition: 'all 1.35s cubic-bezier(0.15, 1, 0.22, 1)'
+                                }}
+                            />
+                        </svg>
+                        Dev. Exp.
+                    </button>
                 </Link>
-
-                <button
-                    className="text-6xl font-bold"
-                    style={{
-                        ...buttonBaseStyle,
-                    }}
-                    onMouseEnter={() => setHoverUser(true)}
-                    onMouseLeave={() => setHoverUser(false)}
-                    onClick={handleClick}
-                >
-                    <svg style={svgStyle}>
-                        <rect
-                            width="100%"
-                            height="100%"
-                            rx="10"
-                            ry="10"
-                            style={{
-                                fill: 'none',
-                                stroke: 'currentColor',
-                                strokeWidth: '4',
-                                strokeDasharray: hoverUser ? '400 0' : '20 320',
-                                strokeDashoffset: hoverUser ? 40 : 100,
-                                transition: 'all 1.35s cubic-bezier(0.19, 1, 0.22, 1)'
-                            }}
-                        />
-                    </svg>
-                    User Exp.
-                </button>
+                <Link href={"/userx"}>
+                    <button
+                        className="text-6xl font-bold"
+                        style={{
+                            ...buttonBaseStyle,
+                        }}
+                        onMouseEnter={() => setHoverUser(true)}
+                        onMouseLeave={() => setHoverUser(false)}
+                        onClick={handleClick}
+                    >
+                        <svg style={svgStyle}>
+                            <rect
+                                width="100%"
+                                height="100%"
+                                rx="10"
+                                ry="10"
+                                style={{
+                                    fill: 'none',
+                                    stroke: 'currentColor',
+                                    strokeWidth: '4',
+                                    strokeDasharray: hoverUser ? '400 0' : '20 320',
+                                    strokeDashoffset: hoverUser ? 40 : 100,
+                                    transition: 'all 1.35s cubic-bezier(0.19, 1, 0.22, 1)'
+                                }}
+                            />
+                        </svg>
+                        User Exp.
+                    </button>
+                </Link>
             </div>
-        </section>
+        </section >
     );
 }
