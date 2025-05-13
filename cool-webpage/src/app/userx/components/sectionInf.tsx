b059ad1ba69ef7c3329e79762abf480930f2ec94
Jorge <jorgechavira4@gmail.com>
2025-05-13 14:22:10 -0600

    stylization fo the section inf

--- cool-webpage/src/app/userx/components/sectionInf.tsx
+++ cool-webpage/src/app/userx/components/sectionInf.tsx
@@ -7,18 +7,34 @@ interface SectionInformationProps {
 
 export default function SectionInformation({ information, bulletTitle, bulletPoints }: SectionInformationProps) {
     return (
-        <div className="h-screen content-center">
-            <div className="w-full h-[10vh] border-2 border-green-500">
+
+        <div>
+            <div
+                style={{
+                    marginTop: 'calc(var(--spacing)*30)',
+                    paddingRight: 'calc(var(--spacing)*10)',
+                    textAlign: 'justify',
+                    marginBottom: 'calc(var(--spacing)*10)'
+                }}>
                 <span>{information}</span>
             </div>
-            <div className="flex items-center justify-evenly w-full border-2 border-blue-500">
+            <div className="flex flex-row "
+                style={{
+                    flexWrap:'wrap',
+                    gap: '40px',
+                    minHeight: '50%',
+                    width:'auto',
+                    border: '1px solid',
+                }}>
                 {bulletPoints.map((pt, i) => (
-                    <div key={i} className=" border-2 border-red-600">
-                        <h2 className=" font-bold">{bulletTitle[i]}</h2>
-                        <ul className="list-disc list-inside">
-                            {pt.map((item, ii) => (
-                                <li key={ii}>{item}</li>
-                            ))}
+                    <div key={i} className=" flex-1"
+                        style={{
+                            border: '1px solid',
+                            padding: '20px'
+                        }}>
+                        <h2 className="font-bold" style={{ margin: '20px 20px 40px 0px' }}>{bulletTitle[i]}</h2>
+                        <ul className="list-disc list-inside" style={{ marginLeft: '20px' }}>
+                            {pt.map((item, ii) => (<li key={ii} >{item}</li>))}
                         </ul>
                     </div>
                 ))}
