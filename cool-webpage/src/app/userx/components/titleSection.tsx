e4eb2ae121e40317dd0b58257e8201ece9144d92
Jorge <jorgechavira4@gmail.com>
2025-05-13 17:24:26 -0600

    title section

--- cool-webpage/src/app/userx/components/titleSection.tsx
+++ cool-webpage/src/app/userx/components/titleSection.tsx
@@ -1,7 +1,12 @@
+interface TitleSectionProps{
+    title:string
+    listTitles:string[]
+} 
 
-export default function TitleSection({ title }: { title: string }) {
+export default function TitleSection({ title, listTitles }: TitleSectionProps) {
     return (
-        <div className=" flex justify-center h-screen" style={{
+        <div className="relative flex justify-center h-screen" 
+        style={{
             width: '20vw',
             minWidth: '200px',
             maxWidth: '400px',
@@ -10,6 +15,10 @@ export default function TitleSection({ title }: { title: string }) {
             <span>
                 <h1 className="text-2xl font-bold">{title} </h1>
             </span>
+
+            <div className="absolute border " style={{bottom:'100px',left:'1rem',paddingLeft:'0.5rem',paddingRight:'0.5rem'}}>
+                {listTitles.map((title,i)=>(<div key={i}>{`[0-0${i}]_${title}`}</div>))}
+            </div>
         </div>
     )
 }
