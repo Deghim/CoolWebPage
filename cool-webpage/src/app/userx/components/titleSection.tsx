a8b760e5cc85d6240f97dd559e8a2982c6114fd4
Jorge <jorgechavira4@gmail.com>
2025-05-13 14:22:32 -0600

    location of the sectoin depending on the screen size

--- cool-webpage/src/app/userx/components/titleSection.tsx
+++ cool-webpage/src/app/userx/components/titleSection.tsx
@@ -1,18 +1,15 @@
 
 export default function TitleSection({ title }: { title: string }) {
     return (
-        <div className=" flex justify-center h-screen w-20 p-30 border border-green-500 ">
+        <div className=" flex justify-center h-screen" style={{
+            width: '20vw',
+            minWidth: '200px',
+            maxWidth: '400px',
+            padding: "100px",
+        }}>
             <span>
                 <h1 className="text-2xl font-bold">{title} </h1>
             </span>
         </div>
     )
 }
-
-/*
-    *  whois      - Who is Jorge?",
-    *  videos     - Visit YouTube channel",
-    *  projects   - Visit GitHub",
-    *  social     - Display social networks",
-    *  secret     - Password required",
- */
