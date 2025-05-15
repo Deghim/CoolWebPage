8ce99063c285ea0a957e991c29facbdb21cc56af
Jorge <jorgechavira4@gmail.com>
2025-05-14 18:49:47 -0600

    estilo del footer

--- cool-webpage/src/app/components/footer.tsx
+++ cool-webpage/src/app/components/footer.tsx
@@ -1,6 +1,8 @@
 'use client';
 
+import Link from "next/link";
 import { useEffect, useRef, useState } from "react";
+import { TypeAnimation } from "react-type-animation";
 
 export default function TheFooter() {
     const [isVisible, setIsVisible] = useState<boolean>(false)
@@ -27,16 +29,49 @@ export default function TheFooter() {
     }, []);
 
     return (
-        <footer ref={footerRef}
-        style={{
-            height:'5vh',
-            display:'flex',
-            borderTop:'2px solid currentColor',
-            justifyContent:'center',
-            alignItems:'center'
+        <footer ref={footerRef} style={{
+            height: '5vh',
+            width: '100vw',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            gap: '5vw',
+            padding: '0',
+            margin: '0'
+
+        }}>
+            <Link href={"/"} style={{
+                height: '100%',
+                minWidth: '200px',
+                width: '20vw',
+                maxWidth: '400px',
+                display: 'flex',
+                borderTop: '2px solid currentColor',
+                justifyContent: 'center',
+                alignItems: 'center',
+                fontWeight: 'bold'
             }}>
-            hola mama
-            {isVisible && <p>I am now in view!</p>}
+                <div>
+                    {/* {isVisible && (<TypeAnimation sequence={['Go Back... ', 1000]} />)} */}
+                    Go Back...
+                </div>
+            </Link>
+            <div style={{
+                height: '100%',
+                width:"100%",
+                display: 'flex',
+                borderTop: '2px solid currentColor',
+                alignItems: 'center',
+                fontWeight: 'bold'
+            }}>
+                {isVisible && (<TypeAnimation sequence={[
+                    `or don't`, 1500,
+                    `or go home`, 1500,
+                    `u gotta click "Go Back..." to go back`, 1500,
+                    `lol`, 1000,
+                ]}
+                    repeat={Infinity} />)}
+            </div>
         </footer>
     );
 }
