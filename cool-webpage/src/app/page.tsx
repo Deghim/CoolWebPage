ed6a7ef4225c1e802c499831dae2027695999c34
Jorge <jorgechavira4@gmail.com>
2026-02-24 13:45:11 -0600

    Cambio de landing de la pagina y simplificacion de codigo para la nueva landing

--- cool-webpage/src/app/page.tsx
+++ cool-webpage/src/app/page.tsx
@@ -1,39 +1,40 @@
 'use client';
-import Landing from "./landing/landing";
-import Options from "./options/options";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import PixelBack from "./pixelBackground/pixelBackground";
+import UserX from "./userx/page";
 
 export default function Home() {
   const [showAnimation, setShowAnimation] = useState(true)
   const [afterShowAnimation, setAfterShowAnimation] = useState(false)
   const [gatillo, setGatillo] = useState(false);
-  const [progreView, setProgreView] = useState(0);
+  const effectRan = useRef(false);
+  // const [progreView, setProgreView] = useState(0);
 
   useEffect(() => {
-    const updateProgre = () => {
-      const scrollY: number = window.scrollY
-      const viewPortH: number = window.innerHeight
+    if (effectRan.current) return;
+    effectRan.current = true
+    // const updateProgre = () => {
+    //   const scrollY: number = window.scrollY
+    //   const viewPortH: number = window.innerHeight
 
-      const raw: number = (scrollY / viewPortH) * 100
-      const pct: number = Math.min(Math.max(raw, 0), 100);
+    //   const raw: number = (scrollY / viewPortH) * 100
+    //   const pct: number = Math.min(Math.max(raw, 0), 100);
 
-      setProgreView(pct)
+    //   setProgreView(pct)
+    // }
+
+    // window.addEventListener("scroll", updateProgre, { passive: true })
+    // updateProgre()
+    console.log("Landing")
+    const tAfterShow = setTimeout(() => setAfterShowAnimation(true), 250)
+    const tGatillo = setTimeout(() => setGatillo((gatillo) => !gatillo), 500)
+    const tHideOverlay = setTimeout(() => setShowAnimation(false), 1500)
+    // return () => window.removeEventListener("scroll", updateProgre)
+    return () => {
+      clearTimeout(tAfterShow)
+      clearTimeout(tGatillo)
+      clearTimeout(tHideOverlay)
     }
-
-    window.addEventListener("scroll", updateProgre, { passive: true })
-    updateProgre()
-    setTimeout(() => {
-      setAfterShowAnimation(true)
-    }, 250)
-
-    setTimeout(() => {
-      setGatillo(!gatillo);
-      setTimeout(() => {
-        setShowAnimation(false);
-      }, 1000)
-    }, 500)
-    return () => window.removeEventListener("scroll", updateProgre)
   }, []);
 
   return (
@@ -41,12 +42,15 @@ export default function Home() {
       {showAnimation && (<div className="fixed inset-0 z-10">
         <PixelBack gatillo={!gatillo} />
       </div>)}
-      <div style={{ opacity: `${afterShowAnimation ? 1 : 0}` }}>
+      {/* <div style={{ opacity: `${afterShowAnimation ? 1 : 0}` }}>
         <div style={{ position: 'fixed', bottom: `${progreView}%`, left: '50%', transform: 'translateX(-50%)', fontSize: '1rem', fontWeight: 'bold', opacity: progreView >= 100 ? 0 : 100, transition: 'opacity 0.5s ease', }}>
           {Math.round(progreView)}%
         </div>
         <Landing />
         <Options opacity={progreView >= 100 ? 100 : 0} />
+      </div> */}
+      <div className={` transition-opacity duration-300 ease-in-out will-change-opacity ${afterShowAnimation ? "opacity-100" : "opacity-0"}`}>
+        <UserX />
       </div>
     </div>
   );
